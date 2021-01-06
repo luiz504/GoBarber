54b4f06e626842e1961220e08ffcdade85e005ce
l.bueno <luiz504@hotmail.com>
2021-01-06 17:03:20 -0300

    eslint/prettier setup

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
-app.use(routes)
+const hello = [];
 
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333, ()=>{
-  console.log('Server On port 3333')
-})
+app.listen(3333, () => {
+  console.log('Server On port 3333');
+});
