cb89070368d97e6d7a35cd9323a1260b990a093d
l.bueno <luiz504@hotmail.com>
2021-01-06 16:23:51 -0300

    integrate vscode debugger

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,11 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes'
 
 const app = express();
 
-app.get(`/`,(request,response) => {
-  return response.json({message:`hello word 3`})
-})
+app.use(express.json())
+app.use(routes)
+
 
 app.listen(3333, ()=>{
   console.log('Server On port 3333')
