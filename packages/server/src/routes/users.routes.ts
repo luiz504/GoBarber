203ec1ffeb47060ad763ee25791add025b07e0a7
l.bueno <luiz504@hotmail.com>
2021-01-08 15:50:05 -0300

    global errors handling

--- packages/server/src/routes/users.routes.ts
+++ packages/server/src/routes/users.routes.ts
@@ -10,23 +10,19 @@ const usersRouter = Router();
 const uploadMiddleware = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    return response.json({
-      name,
-      email,
-      id: user.id,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    });
-  } catch (err) {
-    return response.status(400).json(err.message);
-  }
+  return response.json({
+    name,
+    email,
+    id: user.id,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  });
 });
 
 usersRouter.patch(
@@ -34,25 +30,21 @@ usersRouter.patch(
   ensureAuthenticated,
   uploadMiddleware.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
+    const updateUserAvatarService = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      return response.json({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-        avatar: user.avatar,
-      });
-    } catch (err) {
-      return response.status(400).json(err.message);
-    }
+    return response.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+      avatar: user.avatar,
+    });
   },
 );
 
