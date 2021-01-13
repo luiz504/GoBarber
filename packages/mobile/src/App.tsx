ff2fbd11feff9c713e4183a48cae9751262ea6ec
l.bueno <luiz504@hotmail.com>
2021-01-13 14:23:47 -0300

    Setup Mobile

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text> Hello Word</Text>
+    </View>
+  );
+};
+
+export default App;
