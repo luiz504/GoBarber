24709f62a99b900608a18f3f69c49098e109f2d2
l.bueno <luiz504@hotmail.com>
2021-02-20 17:26:05 -0300

    Tests Appointments backend

--- packages/server/src/modules/appointments/services/CreateAppointment.service.spec.ts
+++ packages/server/src/modules/appointments/services/CreateAppointment.service.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointment.service';
 
@@ -8,14 +9,27 @@ describe('Create Appointment', () => {
       fakeAppointmentRepository,
     );
 
-    const appointmentData = { date: new Date(), provider_id: '111111' };
+    const appointmentData = {
+      date: new Date(),
+      provider_id: '111111',
+    };
 
     const appointment = await createAppointment.execute(appointmentData);
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe(appointmentData.provider_id);
   });
-  it('Should not be able to create two appointments on the same time', () => {
-    expect(1 + 2).toBe(3);
+  it('Should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentData = { date: new Date(), provider_id: '111111' };
+
+    await createAppointment.execute(appointmentData);
+    expect(createAppointment.execute(appointmentData)).rejects.toBeInstanceOf(
+      AppError,
+    );
   });
 });
