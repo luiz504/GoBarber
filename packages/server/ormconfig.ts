e6148e34a9538b32a4582d4f6f279b978fdb8204
l.bueno <luiz504@hotmail.com>
2021-03-12 21:05:01 -0300

    Mongo Interation, Schema Notifications creation

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -1,13 +1,25 @@
-export default {
-  type: 'postgres',
-  host: process.env.ORM_HOST,
-  port: process.env.ORM_PORT,
-  username: process.env.ORM_USERNAME,
-  password: process.env.ORM_PASSWORD,
-  database: process.env.ORM_DATABASE,
-  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
-  cli: {
-    migrationsDir: './src/shared/infra/typeorm/migrations',
+export default [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.ORM_HOST,
+    port: process.env.ORM_PORT,
+    username: process.env.ORM_USERNAME,
+    password: process.env.ORM_PASSWORD,
+    database: process.env.ORM_DATABASE,
+    entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/shared/infra/typeorm/migrations',
+    },
   },
-};
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.ORM2_HOST,
+    port: process.env.ORM2_PORT,
+    database: process.env.ORM2_DATABASE,
+    useUnifiedTopology: true,
+    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+];
