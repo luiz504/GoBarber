136b1ba5d6a44db45ab734abe7554dcd7f97a317
l.bueno <luiz504@hotmail.com>
2021-03-13 09:31:00 -0300

    adjusting envs

---
+++ packages/server/src/@types/modules.d.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    ORM_HOST: string;
+    ORM_PORT: number;
+    ORM_USERNAME: string;
+    ORM_PASSWORD: number;
+    ORM_DATABASE: string;
+
+    ORM2_HOST: string;
+    ORM2_PORT: number;
+    ORM2_DATABASE: string;
+
+    TOKEN_SECRET: string | number;
+    TOKEN_EXPIRATION: string;
+    APP_WEB_URL: string;
+  }
+}
